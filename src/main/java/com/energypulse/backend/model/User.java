2d80228a4ed6da0f8de0d893379ff8fe91712a38
Iresh Dilshan <ireshdilshan@gmail.com>
2026-09-12 11:58:25 +0530

    create contet pathnd user entity

---
+++ src/main/java/com/energypulse/backend/model/User.java
@@ -0,0 +1,43 @@
+package com.energypulse.backend.model;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity 
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Data 
+@Builder 
+@Table (name = "users")
+public class User {
+    @Id 
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private UUID userId;
+    private String username;
+    private String email;
+    private String password;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String HVACType;
+    private String householdOccupants;
+
+    @PrePersist 
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+        userId = UUID.randomUUID();
+    }
+}
